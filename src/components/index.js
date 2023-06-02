337ab9eeed64fbb79220d60fb59f988ce43aa4d1
zhangbinzhbb <13020106399@163.com>
2023-06-02 16:46:53 +0800

    add: 路由验证

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,8 @@ export { default as SvgIcon } from './SvgIcon'
 export { default as Hamburger } from './Hamburger'
 export { default as Dashboard } from '@/views/dashboard'
 export { default as SomeModule } from '@/views/someModule'
+export { default as SomeModuleTest } from '@/views/someModule/test'
+export { default as SomeModuleChild } from '@/views/someModule/child'
+console.log(' test 123')
+// import loadApp from '../loadApp.js'
+// export default loadApp
--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import { LIB_NAME } from './configs/subApp'
 import { libReady, isMasterApp } from 'hel-lib-proxy'
-
+console.log(' test1234-main ')
 async function main() {
   if (isMasterApp()) {
     await import('./loadApp')
   } else {
+    console.log(' test12345-sub-main ')
     const libProperties = await import('./entrance/libProperties')
     libReady(LIB_NAME, libProperties.default)
   }
