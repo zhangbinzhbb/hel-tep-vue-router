f4f21d1a3e4d4388295ac33b448ebb96e886a3f5
[zhangbiaobin] <[zhangbiaobin@leyaoyao.com]>
2023-08-25 15:07:24 +0800

    add: micro-app组件

--- src/components/index.js
+++ src/components/index.js
@@ -6,8 +6,9 @@ console.log(' test 123')
 export { default as SomeModule } from '@/views/someModule'
 export { default as SomeModuleTest } from '@/views/someModule/test'
 export { default as SomeModuleChild } from '@/views/someModule/child'
+export { default as MicroAppTest } from '@/views/microAppTest/index'
 // export { default as App } from '../App.vue'
 
-console.log(' test 123')
+console.log(' test 1234')
 // import loadApp from '../loadApp.js'
 // export default loadApp
