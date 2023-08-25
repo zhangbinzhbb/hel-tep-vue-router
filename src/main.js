e55a0aeded4741a57a0f66147acee093e209bd8a
[zhangbiaobin] <[zhangbiaobin@leyaoyao.com]>
2023-08-25 14:42:54 +0800

    add: main

--- src/main.js
+++ src/main.js
@@ -7,12 +7,12 @@ import { LIB_NAME } from './configs/subApp'
   console.log('====子应用======')
   console.log('isMasterApp()====>', isMasterApp())
   const libProperties = await import('./entrance/libProperties')
-  // 注意此处传递的是 default
+  // 注意此处传递的是 default ，发射模块
   libReady(LIB_NAME, libProperties.default)
 
   // 非子应用时（即不是被别的模块触发载入的情况），自己挂载渲染节点，方便本地调试
   // 可根据项目实际情况控制是否载入 loadApp 文件
-
+  // 当前模块作为宿主被加载时
   if (isMasterApp()) {
     console.log('====子应用=123=====')
     await import('./loadApp')
